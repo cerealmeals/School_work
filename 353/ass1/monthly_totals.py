d2e8dd9abd620922c30b3d92f2f909a2df097c2f
Sam Nygard <samnygard@gmail.com>
2024-01-19 19:03:44 -0800

    part 3 done

---
+++ 353/ass1/monthly_totals.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+def date_to_month(d):
+    # You may need to modify this function, depending on your data types.
+    list = d.split('-')
+    return list[0] + '-' + list[1]
+
+def pivot_months_pandas(data):
+    """
+    Create monthly precipitation totals for each station in the data set.
+    
+    This should use Pandas methods to manipulate the data.
+    """
+    # ...
+    monthly = data.groupby(['name', 'month']).agg({'precipitation' : ['sum']}).reset_index()
+    monthly['sum'] = monthly['precipitation']
+    monthly = monthly.pivot(index='name', columns='month', values='sum')
+    print(monthly)
+
+    counts = data.groupby(['name', 'month']).agg({'precipitation' : ['count']}).reset_index()
+    counts['count'] = counts['precipitation']
+    counts = counts.pivot(index='name', columns='month', values='count')
+    print(counts)
+
+
+    return monthly, counts
+
+
+data = pd.read_csv('353\\ass1\\e1\\precipitation.csv')
+
+data['month'] = data['date'].map(lambda x: date_to_month(x))
+#print(data)
+
+totals, counts = pivot_months_pandas(data)
+totals.to_csv('totals.csv')
+counts.to_csv('counts.csv')
+np.savez('monthdata.npz', totals=totals.values, counts=counts.values)
+
