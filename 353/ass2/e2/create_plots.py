9f54175a0cb62447bfa4653ab73538103d40563c
Sam -VM-desktop <samnygard@gmail.com>
2024-01-22 09:46:18 -0800

    back to laptop for 353

--- 353/ass2/e2/create_plots.py
+++ 353/ass2/e2/create_plots.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 filename1 = sys.argv[1]
 filename2 = sys.argv[2]
@@ -13,4 +13,16 @@ file2 = pd.read_csv(filename2, sep=' ', header=None, index_col=1,
         names=['lang', 'page', 'views', 'bytes'])
 
 print(file1)
-print(file2)
+print(file1.columns)
+#print(file2)
+
+sorted_file1 = file1['views'].sort_values(ascending=False)
+
+#file1.sort_values('views', axis=1, ascending=False)
+
+plt.figure(figsize=(10, 5)) # change the size to something sensible
+plt.subplot(1, 2, 1) # subplots in 1 row, 2 columns, select the first
+plt.plot(sorted_file1.values) # build plot 1
+# plt.subplot(1, 2, 2) # ... and then select the second
+# plt.plot(…) # build plot 2
+plt.show()
