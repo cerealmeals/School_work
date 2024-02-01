7a91ecf7228875cceda6c66644203e2d7bc1c512
= <=>
2024-01-31 16:28:42 -0800

    just a commit before going to class

--- 353/ass3/e3/smooth_temperature.py
+++ 353/ass3/e3/smooth_temperature.py
@@ -1,23 +1,45 @@
-#import sys
+import sys
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-import re
+import matplotlib.pyplot as plt
+
 from datetime import datetime
 from scipy import stats
+from statsmodels.nonparametric.smoothers_lowess import lowess
+from pykalman import KalmanFilter
 
-#filename1 = sys.argv[1]
-#cpu_data = pd.read_csv(filename1)
+filename1 = sys.argv[1]
+cpu_data = pd.read_csv(filename1)
 
-cpu_data = pd.read_csv('sysinfo.csv')
+float_time = cpu_data['timestamp'].apply(lambda x: datetime.timestamp(datetime.strptime(x,"%Y-%m-%d %H:%M:%S.%f")))
+loess_smoothed = lowess(cpu_data['temperature'].values, float_time, frac = 0.05)
 
-# plt.figure(figsize=(12, 4))
-# plt.plot(cpu_data['timestamp'], cpu_data['temperature'], 'b.', alpha=0.5)
-# plt.show() # maybe easier for testing
-# plt.savefig('cpu.svg') # for final submission
 
-print(cpu_data)
+kalman_data = cpu_data[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
 
-filtered = stats.nonparametric.smoothers_lowess.lowess(cpu_data['temperature'].values, cpu_data['timestamp'])
+initial_state = kalman_data.iloc[0]
+observation_covariance = np.diag([3, 2, 2, 50]) ** 2 # TODO: shouldn't be zero
+transition_covariance = np.diag([0.01, 0.01, 0.05, 0.05]) ** 2 # TODO: shouldn't be zero
+transition = [[0.97,0.5,0.2,00.001], [0.1,0.4,2.2,0], [0,0,0.95,0], [0,0,0,1]] # TODO: shouldn't (all) be zero
 
-print(filtered)
+kf = KalmanFilter(
+    initial_state_mean=initial_state,
+    initial_state_covariance=observation_covariance,
+    observation_covariance=observation_covariance,
+    transition_covariance=transition_covariance,
+    transition_matrices=transition
+)
+
+pred_state, state_cov = kf.smooth(kalman_data)
+
+plt.figure(figsize=(12, 4))
+plt.xticks(range(0, len(cpu_data['timestamp'].values), 300), rotation=25)
+plt.plot(cpu_data['timestamp'], cpu_data['temperature'], 'b.', alpha=0.5)
+plt.plot(cpu_data['timestamp'], loess_smoothed[:, 1], 'r-', linewidth=2)
+plt.plot(cpu_data['timestamp'], pred_state[:, 0], 'g-', linewidth=2)
+plt.legend(['Sensor reading', 'LOESS-smoothed', 'Kalman-smoothed'])
+plt.xlabel('Time')
+plt.ylabel('CPU Temperature')
+#plt.show() # maybe easier for testing
+
+plt.savefig('cpu.svg') # for final submission
