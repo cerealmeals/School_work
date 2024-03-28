9244dd06b6aa264e644d264b8fc40930d9273dad
= <=>
2024-03-27 21:23:41 -0700

    353 e10 stuff

--- 353/e10/wikipedia_popular.py
+++ 353/e10/wikipedia_popular.py
@@ -10,17 +10,40 @@ assert spark.version >= '2.3' # make sure we have Spark 2.3+
 
 comments_schema = types.StructType([
     types.StructField('language', types.StringType()),
-    types.StructField('page_name', types.StringType()),
+    types.StructField('title', types.StringType()),
     types.StructField('visits', types.LongType()),
     types.StructField('bytes', types.LongType()),
     #types.StructField('year', types.IntegerType()),
     #types.StructField('month', types.IntegerType()),
 ])
 
+def filename_to_date_hour(string):
+    output = string[-18:-7]
+    #output = output[:len(output)-4]
+    return output
+
 
 def main(in_directory, out_directory):
-    pages = spark.read.option("delimiter", " ").csv(in_directory, schema=comments_schema)
-    pages.show(); return
+    pages = spark.read.option("delimiter", " ").csv(in_directory, schema=comments_schema).withColumn('filename', functions.input_file_name())
+
+    pages = pages.where(pages.language == 'en')
+    pages = pages.where(pages.title != 'Main_Page')
+    Specials = pages.where((pages.title.startswith('Special:')))
+
+    pages = pages.subtract(Specials)
+    
+    file_to_date = functions.udf(filename_to_date_hour, returnType=types.StringType())
+
+    pages = pages.withColumn('filename', file_to_date(pages.filename))
+    pages = pages.drop('bytes').drop('language')
+
+    groups = pages.groupby('filename').max('visits')
+    groups 
+
+    groups.write.csv(out_directory, mode='overwrite')
+    groups.show(); return
+
+
 
 
 if __name__=='__main__':
