1497953c858a88d37a665af2c7277a17e64f1e5a
Sam Nygard <samnygard@gmail.com>
2024-04-10 15:29:31 -0700

    changes to 353 and 361

--- 353/e12/dogplot.py
+++ 353/e12/dogplot.py
@@ -39,19 +39,30 @@ print('Intercept:',fit.intercept)
 print('Pvalue:', fit.pvalue)
 
 
-tweets = tweets.sort_values(by=['timestamp'])
-before_2017 = tweets[(tweets['created_at']<= '2017-01-01 00:00:01')]
-after_2017 = tweets[(tweets['created_at'] > '2017-01-01 00:00:01')]
+# tweets = tweets.sort_values(by=['timestamp'])
+# before_2017 = tweets[(tweets['created_at']<= '2017-01-01 00:00:01')]
+# after_2017 = tweets[(tweets['created_at'] > '2017-01-01 00:00:01')]
 
-above_10_pre_2017 = before_2017[(before_2017['ratings'] > 10)]
-below_10_pre_2017 = before_2017[(before_2017['ratings'] <= 10)]
-percent_above_10_pre_2017 = above_10_pre_2017['ratings'].count() / (above_10_pre_2017['ratings'].count() + below_10_pre_2017['ratings'].count())
-print('Percert above 10 before 2017', percent_above_10_pre_2017)
+# above_10_pre_2017 = before_2017[(before_2017['ratings'] > 10)]
+# below_10_pre_2017 = before_2017[(before_2017['ratings'] <= 10)]
+# percent_above_10_pre_2017 = above_10_pre_2017['ratings'].count() / (above_10_pre_2017['ratings'].count() + below_10_pre_2017['ratings'].count())
+# print('Percert above 10 before 2017', percent_above_10_pre_2017)
 
-above_10_after_2017 = after_2017[(after_2017['ratings'] > 10)]
-below_10_after_2017 = after_2017[(after_2017['ratings'] <= 10)]
-percent_above_10_after_2017 = above_10_after_2017['ratings'].count() / (above_10_after_2017['ratings'].count() + below_10_after_2017['ratings'].count())
-print('Percert above 10 after 2017', percent_above_10_after_2017)
+# above_10_after_2017 = after_2017[(after_2017['ratings'] > 10)]
+# below_10_after_2017 = after_2017[(after_2017['ratings'] <= 10)]
+# percent_above_10_after_2017 = above_10_after_2017['ratings'].count() / (above_10_after_2017['ratings'].count() + below_10_after_2017['ratings'].count())
+# print('Percert above 10 after 2017', percent_above_10_after_2017)
+
+plt.figure(figsize=(10, 6))
+residuals = tweets['ratings'] - tweets['predictions']
+plt.plot(tweets['created_at'], residuals, marker='o', linestyle='None')
+plt.axhline(y=0, color='r', linestyle='--')
+plt.xlabel('Date')
+plt.ylabel('Residuals')
+plt.title('Residual Plot')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
 
 # residuals = tweets['ratings'].values - (fit.slope*tweets['timestamp'].values + fit.intercept)
 # counts, bins = np.histogram(residuals)
